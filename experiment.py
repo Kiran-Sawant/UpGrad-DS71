bdacf7063892d1ea481efa168a9990ad203d6f97
Kiran-Sawant <kbsawant1005@gmail.com>
2024-12-07 20:02:00 +0530

    x

--- experiment.py
+++ experiment.py
@@ -1,17 +1,6 @@
 import numpy as np
 import pandas as pd
 
-# The file is stored at the following path:
-# 'https://media-doselect.s3.amazonaws.com/generic/A08MajL8qN4rq72EpVJbAP1Rw/marks_1.csv'
-# Provide your answer below
-column = ['S.No.', 'Name', 'Subject', 'Maximun Marks', 'Marks Obtained', 'Percentage']
+k = pd.to_datetime('2013-03-31 00:00:00')
 
-df = pd.read_csv("https://media-doselect.s3.amazonaws.com/generic/A08MajL8qN4rq72EpVJbAP1Rw/marks_1.csv", delimiter='|', header=None)
-df.columns = column
-
-df.set_index(['S.No.'], inplace=True)
-
-print(df)
-
-# Experimenting with GitHub
-# Ok, this works
+print(k.month)
